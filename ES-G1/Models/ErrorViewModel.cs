3c6d322a6cd53b4eabe921cd35b7e574b5b4c553
Stefanospi <sspitaleri26@gmail.com>
2024-07-01 16:11:12 +0200

    ES-G1

---
+++ ES-G1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ES_G1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
